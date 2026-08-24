b800b10c1681065276d3120329a430f1e40caf60
Santanu <santanubhadra21@gmail.com>
2026-08-25 00:55:14 +0530

    Remove & Clear Items

--- 08-shopping-list-project/script.js
+++ 08-shopping-list-project/script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearAll = document.getElementById('clear');
 
 function addItem(e) {
     e.preventDefault();
@@ -33,4 +34,18 @@ function createIcon(classes) {
     return icon;
 }
 
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('text-red')) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearAll.addEventListener('click', clearItems);
