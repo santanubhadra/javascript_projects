f79941c31686da1e17a1f4b3dd19feb3be111e21
Santanu <santanubhadra21@gmail.com>
2026-08-29 23:16:40 +0530

    Project Update 1

--- 08-shopping-list-project/script.js
+++ 08-shopping-list-project/script.js
@@ -34,6 +34,11 @@ function addItem(e) {
         }
     }
 
+    if (checkIfItemExists(newItem)) {
+            alert('Item already exists!');
+            return;
+        }
+
     addItemToDOM(newItem);
 
     addItemToStorage(newItem);
